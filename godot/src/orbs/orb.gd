bee278ed1a3eeea9e3424f7417507131ec14b98c
IceTyp <icetyp@protonmail.com>
2023-08-06 22:44:11 +0200

    move orbs to folder and use inheritance

---
+++ godot/src/orbs/orb.gd
@@ -0,0 +1,37 @@
+@tool
+extends Area2D
+
+@export var energy: int = 1: set = set_energy
+@export var color: Color = Color.WHITE: set = set_color
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var point_light_2d: PointLight2D = $PointLight2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func set_color(_color: Color) -> void:
+	color = _color
+#	if Engine.is_editor_hint():
+#		point_light_2d.color = color
+	await ready
+	point_light_2d.color = color
+
+
+func set_energy(val: float) -> void:
+	@warning_ignore("narrowing_conversion")
+	energy = val
+#	if Engine.is_editor_hint():
+#		point_light_2d.energy = val / 10
+#		animation_player.speed_scale = val / 5
+#		point_light_2d.texture.width = 16 * val
+#		point_light_2d.texture.height = 16 * val
+	await ready
+	point_light_2d.energy = val / 10
+	animation_player.speed_scale = val / 5
+	point_light_2d.texture.width = 16 * val
+	point_light_2d.texture.height = 16 * val
+
+
+func _on_darkness_entered(_body: Node2D) -> void:
+	# TODO add puff animation
+	queue_free()
