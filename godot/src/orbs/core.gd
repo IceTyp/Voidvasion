61d7b2607224f7be9eda5911611b79d30e90c7e1
IceTyp <icetyp@protonmail.com>
2023-08-09 23:59:20 +0200

    wait longer after core broke to end game

--- godot/src/orbs/core.gd
+++ godot/src/orbs/core.gd
@@ -5,9 +5,9 @@ signal core_broken
 
 func _ready() -> void:
 	core_broken.connect(Global._on_core_broken)
+	tree_exiting.connect(_on_tree_exiting)
 	super._ready()
 
 
-func _on_darkness_entered(_body: Node2D) -> void:
+func _on_tree_exiting() -> void:
 	core_broken.emit()
-	super._on_darkness_entered(_body)
