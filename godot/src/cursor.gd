2dcc2ecd5967c0f4cd55db0309ccd4973047e518
IceTyp <icetyp@protonmail.com>
2023-08-09 14:17:09 +0200

    add cursor for placing orbs

---
+++ godot/src/cursor.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+signal place_orb(position)
+
+var small_orb := preload("res://src/orbs/small_orb.tscn")
+var cursor_orb := preload("res://src/orbs/cursor_orb.tscn")
+
+@onready var point_light_2d: PointLight2D = $PointLight2D
+@onready var collision_detector: Area2D = $CollisionDetector
+
+
+func _process(_delta: float) -> void:
+	position = get_global_mouse_position().round()
+	point_light_2d.visible = not collision_detector.has_overlapping_bodies()
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			if collision_detector.has_overlapping_bodies():
+				return
+			else:
+				place_orb.emit(position)
