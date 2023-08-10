eb0ca464687ac413ce300fb8f880793b8d730afe
IceTyp <icetyp@protonmail.com>
2023-08-10 17:28:00 +0200

    make sound effects louder

--- godot/src/maps/map.gd
+++ godot/src/maps/map.gd
@@ -13,4 +13,4 @@ func start_darkening() -> void:
 	var tween := create_tween()
 	tween.tween_property(canvas_modulate, "color", Color(0.5, 0.5, 0.5, 1), 1)
 	tween.tween_property(canvas_modulate, "color", Color(0, 0, 0, 1), 40)
-	tween.parallel().tween_property(audio_stream_player_darkness, "volume_db", 0, 180)
+	tween.parallel().tween_property(audio_stream_player_darkness, "volume_db", 10, 180)
