955b77aa8fb504d2398bdb198c6a19fb76101202
IceTyp <icetyp@protonmail.com>
2023-08-10 18:24:39 +0200

    allow placing orbs closer to darkness

--- godot/src/maps/cursor.gd
+++ godot/src/maps/cursor.gd
@@ -3,7 +3,8 @@ extends Node2D
 signal place_orb(position)
 
 @onready var point_light_2d: PointLight2D = $PointLight2D
-@onready var collision_detector: Area2D = $CollisionDetector
+@onready var orb_detector: Area2D = $OrbDetector
+@onready var darkness_detector: Area2D = $DarknessDetector
 @onready var audio_stream_player_place_orb: AudioStreamPlayer2D = $AudioStreamPlayerPlaceOrb
 @onready var audio_stream_player_failed: AudioStreamPlayer2D = $AudioStreamPlayerFailed
 
@@ -11,7 +12,7 @@ signal place_orb(position)
 func _process(_delta: float) -> void:
 	position = get_global_mouse_position().round()
 	visible = Global.orb_counter < Global.max_orb_number
-	point_light_2d.visible = not collision_detector.has_overlapping_bodies()
+	point_light_2d.visible = is_valid_orb_position()
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -19,7 +20,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			if not get_viewport_rect().has_point(event.position):
 				return
-			if collision_detector.has_overlapping_bodies():
+			if not is_valid_orb_position():
 				audio_stream_player_failed.play()
 			elif Global.orb_counter >= Global.max_orb_number:
 				audio_stream_player_failed.play()
@@ -27,3 +28,7 @@ func _unhandled_input(event: InputEvent) -> void:
 				audio_stream_player_place_orb.play()
 				place_orb.emit(position)
 				get_viewport().set_input_as_handled()
+
+
+func is_valid_orb_position() -> bool:
+	return not orb_detector.has_overlapping_bodies() and not darkness_detector.has_overlapping_bodies()
