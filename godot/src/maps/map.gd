253ec6b0e76a28439e26bc68d8b79c36a2c2a782
IceTyp <icetyp@protonmail.com>
2023-08-10 16:15:13 +0200

    add sound for approaching darkness

--- godot/src/maps/map.gd
+++ godot/src/maps/map.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var canvas_modulate: CanvasModulate = $CanvasModulate
+@onready var audio_stream_player_darkness: AudioStreamPlayer = $AudioStreamPlayerDarkness
 
 
 func _ready() -> void:
@@ -8,6 +9,8 @@ func _ready() -> void:
 
 
 func start_darkening() -> void:
+	audio_stream_player_darkness.play()
 	var tween := create_tween()
 	tween.tween_property(canvas_modulate, "color", Color(0.5, 0.5, 0.5, 1), 1)
 	tween.tween_property(canvas_modulate, "color", Color(0, 0, 0, 1), 40)
+	tween.parallel().tween_property(audio_stream_player_darkness, "volume_db", 0, 180)
