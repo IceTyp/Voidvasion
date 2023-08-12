7032cd9c424d2df2815218d8f4c3268a736d19c0
IceTyp <icetyp@protonmail.com>
2023-08-13 01:24:41 +0200

    add flashing light to core when it breaks

--- godot/src/orbs/core.gd
+++ godot/src/orbs/core.gd
@@ -4,3 +4,14 @@ extends Orb
 func _ready() -> void:
 	orb_broken.connect(Global._on_core_broken)
 	super._ready()
+
+
+func _on_darkness_entered(_body: Node2D) -> void:
+	darkness_detector.body_entered.disconnect(_on_darkness_entered)
+	orbits.queue_free()
+	animation_player.play("super_nova")
+	await animation_player.animation_finished
+	animation_player.play("shatter")
+	await animation_player.animation_finished
+	orb_broken.emit()
+	queue_free()
