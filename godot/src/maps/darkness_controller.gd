8d142c81fe745679b2598d98df953ee3c70943b0
Arytherias <icetyp@protonmail.com>
2023-08-14 10:49:49 +0200

    attach darkness controller directly to tilemap

--- godot/src/maps/darkness_controller.gd
+++ godot/src/maps/darkness_controller.gd
@@ -1,14 +1,11 @@
-extends Node
+extends TileMap
 
 const TILEMAP_LAYER := 0
 const TILESET_SRC_ID := 0
 const DARKNESS_ALT_TILE_ID := 2
 
-@export_node_path("TileMap") var tile_map_path := ^".."
 @export var upper_left_corner := Vector2i(0, 7)
 @export var lower_right_corner := Vector2i(63, 63)
-
-@onready var tile_map := get_node(tile_map_path) as TileMap
 
 
 func _ready() -> void:
@@ -21,20 +18,19 @@ func _ready() -> void:
 ##
 ## param alt_tile_id: the alternative_tile id of the tile used for filling
 func fill_empty_cells(alt_tile_id: int) -> void:
-	var used_cells := tile_map.get_used_cells(TILEMAP_LAYER)
-	used_cells.sort()
+	var used_cells := get_used_cells(TILEMAP_LAYER)
 	
 	for cell in used_cells:
 		upper_left_corner.x = mini(upper_left_corner.x, cell.x)
 		upper_left_corner.y = mini(upper_left_corner.y, cell.y)
 		lower_right_corner.x = maxi(lower_right_corner.x, cell.x)
 		lower_right_corner.y = maxi(lower_right_corner.y, cell.y)
-
+	
 	for x in range(upper_left_corner.x, lower_right_corner.x + 1):
 		for y in range(upper_left_corner.y, lower_right_corner.y + 1):
 			var coord := Vector2i(x,y)
 			if coord not in used_cells:
-				tile_map.set_cell(
+				set_cell(
 						TILEMAP_LAYER, 
 						coord, 
 						TILESET_SRC_ID, 
