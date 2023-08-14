95fa4cf5adfdb5944f48c8531b04b20cbc7eab13
Arytherias <icetyp@protonmail.com>
2023-08-15 01:08:49 +0200

    update game export settings

--- game_export.sh
+++ game_export.sh
@@ -10,18 +10,32 @@ else
     godot --headless --quiet --path godot/ --export-release "Windows Desktop" ../exports/windows/$file_name/Voidvasion.exe
     cp README.md exports/windows/$file_name
     cd exports/windows
-    zip -r $file_name.zip $file_name
+    zip -r ../${file_name}_windows.zip $file_name
     cd ../..
 fi
 
-# if [ -e "exports/linux/$file_name" ]
-# then
-#     echo Version $version already exists for Linux. Nothing exported.
-# else
-#     mkdir -p exports/linux/$file_name
-#     echo Using Godot $(godot --version)
-#     godot --headless --quiet --path godot/ --export-release "Linux/X11" ../exports/linux/$file_name/Voidvasion.x86_64
-#     cp README.md exports/linux/$file_name
-#     cd exports/linux
-#     tar -czvf $file_name.tar.gz $file_name
-# fi
+if [ -e "exports/linux/$file_name" ]
+then
+    echo Version $version already exists for Linux. Nothing exported.
+else
+    mkdir -p exports/linux/$file_name
+    echo Using Godot $(godot --version)
+    godot --headless --quiet --path godot/ --export-release "Linux/X11" ../exports/linux/$file_name/Voidvasion.x86_64
+    cp README.md exports/linux/$file_name
+    cd exports/linux
+    tar -czvf ../${file_name}_linux.tar.gz $file_name
+    cd ../..
+fi
+
+if [ -e "exports/web/$file_name" ]
+then
+    echo Version $version already exists for Web. Nothing exported.
+else
+    mkdir -p exports/web/$file_name
+    echo Using Godot $(godot --version)
+    godot --headless --quiet --path godot/ --export-release "Web" ../exports/web/$file_name/index.html
+    cp README.md exports/web/$file_name
+    cd exports/web
+    zip -r ../${file_name}_web.zip $file_name
+    cd ../..
+fi
