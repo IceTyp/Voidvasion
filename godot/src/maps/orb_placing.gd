3266410c9dbe534997a0ccd936eeee27ef1d866b
Arytherias <icetyp@protonmail.com>
2023-08-17 21:48:07 +0200

    rename cursor to cursor light

--- godot/src/maps/orb_placing.gd
+++ godot/src/maps/orb_placing.gd
@@ -3,7 +3,7 @@ extends Node2D
 var small_orb := preload("res://src/orbs/small_orb.tscn")
 
 
-func _on_cursor_place_orb(_position: Vector2) -> void:
+func _on_cursor_light_place_orb(_position: Vector2) -> void:
 	var orb := small_orb.instantiate()
 	orb.position = _position.round()
 	add_child(orb)
